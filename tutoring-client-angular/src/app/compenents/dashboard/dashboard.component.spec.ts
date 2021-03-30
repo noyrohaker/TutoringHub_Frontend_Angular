c36d8678bc005bfe04e9c26dc696330d65940c87
noyrohaker <noyrohaker98@gmail.com>
2021-03-30 13:36:58 +0300

    Changes in dashboard

--- tutoring-client-angular/src/app/compenents/dashboard/dashboard.component.spec.ts
+++ tutoring-client-angular/src/app/compenents/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FilterPipe } from './filter.pipe';
 import { DashboardComponent } from './dashboard.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,9 +11,8 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
+      declarations: [DashboardComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -23,3 +25,35 @@ describe('DashboardComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+// import { TestBed, async } from '@angular/core/testing';
+// import { DashboardComponent } from './dashboard.component';
+
+// describe('AppComponent', () => {
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [DashboardComponent],
+//     }).compileComponents();
+//   }));
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(DashboardComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   it(`should have as title 'angular-text-search-highlight'`, () => {
+//     const fixture = TestBed.createComponent(DashboardComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app.title).toEqual('angular-text-search-highlight');
+//   });
+
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(DashboardComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.debugElement.nativeElement;
+//     expect(compiled.querySelector('.content span').textContent).toContain(
+//       'angular-text-search-highlight app is running!'
+//     );
+//   });
+// });
--- tutoring-client-angular/src/app/compenents/dashboard/dashboard.component.ts
+++ tutoring-client-angular/src/app/compenents/dashboard/dashboard.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+// import { AppModule } from 'src/app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+  constructor() {}
 
-  constructor() { }
+  title = 'angular-text-search-highlight';
+  searchText = '';
 
-  ngOnInit(): void {
-  }
-
+  teachersArray = [
+    {
+      teacherName: 'Noy Rohaker',
+      city: 'Kiryat Ono',
+      classType: 'Math',
+      ageRange: '16-20',
+    },
+    {
+      teacherName: 'Gal Blond',
+      city: 'Tel Aviv',
+      classType: 'Math',
+      ageRange: '10-20',
+    },
+    {
+      teacherName: 'Adi Hahamov',
+      city: 'Holon',
+      classType: 'Math',
+      ageRange: '14-21',
+    },
+    {
+      teacherName: 'Alona Haimov',
+      city: 'Ashdod',
+      classType: 'Math',
+      ageRange: '17-20',
+    },
+  ];
 }
